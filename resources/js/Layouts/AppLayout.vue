9c67f681c08f41ea159487c8742de40f80ac1e91
Arcylisz <olek1305@gmail.com>
2024-11-28 15:31:41 +0100

    unnecessary

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,18 +1,14 @@
 <script setup>
-import { ref } from 'vue';
 import { Head, Link, router } from '@inertiajs/vue3';
 import ApplicationMark from '@/Components/ApplicationMark.vue';
 import Banner from '@/Components/Banner.vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 
 defineProps({
     title: String,
 });
-
-const showingNavigationDropdown = ref(false);
 
 const logout = () => {
     router.post(route('logout'));
