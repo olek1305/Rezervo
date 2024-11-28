9d1b6eb7e5a78dcc7d60bc5affe6258869c43f78
Arcylisz <olek1305@gmail.com>
2024-11-28 17:11:29 +0100

    update seeder users, doctors and admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         User::factory(10)->create();
+        User::factory(10)->create();
 
-//        User::factory()->create([
-//            'name' => 'Test User',
-//            'email' => 'test@example.com',
-//        ]);
+        // Create 5 admins
+        User::factory(5)->create([
+            'role' => 'admin',
+        ]);
+
+        // Create 5 doctors
+        User::factory(5)->create([
+            'role' => 'doctor',
+        ]);
+
+        //User
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        //Admin
+        User::factory()->create([
+            'name' => 'Test Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin',
+        ]);
+
+        //Doctor
+        User::factory()->create([
+            'name' => 'doctor User',
+            'email' => 'doctor@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'doctor',
+        ]);
     }
 }
