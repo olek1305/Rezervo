9931b3146a04bdee824919fb322023716691d199
Aleksander Żak <51216445+olek1305@users.noreply.github.com>
2024-12-12 16:06:26 +0100

    Fix UI

--- resources/js/Components/LanguageSwitcher.vue
+++ resources/js/Components/LanguageSwitcher.vue
@@ -2,7 +2,7 @@
     <div class="language-switcher">
         <select
             id="language"
-            class="inline-flex items-center px-4 py-2 w-28 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-50 transition ease-in-out duration-150"
+            class="px-3 py-2 text-sm font-medium text-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-400 w-24"
             @change="changeLanguage($event)"
         >
             <option value="en" :selected="locale === 'en'">English</option>
