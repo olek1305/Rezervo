e100b0834f7f60e2f80977e115daf5241f7326d3
Aleksander Żak <51216445+olek1305@users.noreply.github.com>
2024-12-16 12:02:16 +0100

    remove px and py, increase width

--- resources/js/Components/LanguageSwitcher.vue
+++ resources/js/Components/LanguageSwitcher.vue
@@ -2,7 +2,7 @@
     <div class="language-switcher">
         <select
             id="language"
-            class="px-3 py-2 text-sm font-medium text-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-400 w-24"
+            class="text-sm font-medium text-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-400 w-28"
             @change="changeLanguage($event)"
         >
             <option value="en" :selected="locale === 'en'">English</option>
